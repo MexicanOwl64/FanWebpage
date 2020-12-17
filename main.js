3cf0745bc89d466ce1822cd2f0c8904e4311eea2
MexicanOwl64 <55261799+MexicanOwl64@users.noreply.github.com>
2020-12-17 00:22:44 -0600

    The website is done
    The final Website is done.  All it needs is to do a few more test to make sure everything works as intended.

---
+++ main.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+
+  $('#MarioShow').click(function(){
+    $('#textMario-show').show()
+    $('#MarioImg-show').hide()
+    $('#bringMario-show').show()
+  
+  })
+  
+  
+  $('#MarioHide').click(function(){
+    $('#textMario-show').hide();
+    $('#MarioImg-show').show();
+    $('#bringMario-show').hide();
+  
+  })
+
+  $('#ZeldaShow').click(function(){
+    $('#textZelda-show').show()
+    $('#ZeldaImg-show').hide()
+    $('#bringZelda-show').show()
+  
+  })
+  
+  
+  $('#ZeldaHide').click(function(){
+    $('#textZelda-show').hide();
+    $('#ZeldaImg-show').show();
+    $('#bringZelda-show').hide();
+  
+  })
+
+  $('#PokemonShow').click(function(){
+    $('#textPokemon-show').show()
+    $('#PokemonImg-show').hide()
+    $('#bringPokemon-show').show()
+  
+  })
+  
+  
+  $('#PokemonHide').click(function(){
+    $('#textPokemon-show').hide();
+    $('#PokemonImg-show').show();
+    $('#bringPokemon-show').hide();
+  
+  })
+  
+  });
+  
+  
